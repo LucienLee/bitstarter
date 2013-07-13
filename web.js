ba79af35ed2393d6a213ab1b54db8976b9b8e0b2
Lucien Lee <lkiral7903@gmail.com>
2013-07-13 17:22:45 +0000

    fix

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var buffer;
-    fs.readFileSync('index.html',function(err, data){
+    fs.readFileSync('index.html','utf-8' , function(err, data){
 	if(err) throw err;
 	buffer = new Buffer(data,"utf-8");
     });
